8f09ae14de67d7b7f964a5ae0192f7b48cd51311
王润心 <wangrunxinyes@gmail.com>
2015-11-09 21:16:03 +0800

    -test

--- frontend/desktop/views/wang/search.php
+++ frontend/desktop/views/wang/search.php
@@ -9,10 +9,10 @@
 // 113°46'～114°37
 // 22°27'～22°52'
 set_time_limit(0);
-$location_a_x = 547;
-$location_a_y = 942;
-$location_b_x = 548;
-$location_b_y = 943;
+$location_a_x = 54752;
+$location_a_y = 12532;
+$location_b_x = 54758;
+$location_b_y = 12536;
 $category = urlencode("百货");
 $store_category = "购物广场";
 $i = 0;
@@ -29,18 +29,23 @@ $type = array(
 	'美食' => '餐饮',
 );
 
+echo $_SERVER['DOCUMENT_ROOT']."/";
+exit;
 foreach ($type as $key => $value) {
 	$category = urlencode($key);
 	$store_category = $value;
 
 	Logger::log("category: " . urldecode($category));
 	$i = 0;
+
+	pclose(popen(" &", 'r'));
+	continue;
 	while ($i < 40) {
 		$j = 0;
 		while ($j < 40) {
 
-			$bounds = "22." . $location_a_x . "," . "113." . $location_a_y . ","
-				. "22." . $location_b_x . "," . "113." . $location_b_y;
+			$bounds = "22." . $location_a_x . "," . "114." . $location_a_y . ","
+				. "22." . $location_b_x . "," . "114." . $location_b_y;
 			$url = 'http://api.map.baidu.com/place/v2/search?query=' . $category
 				. '&bounds=' . $bounds
 				. '&output=json&ak=X4QYNSSzEH46X7xp10GkCIxF';
@@ -51,12 +56,12 @@ foreach ($type as $key => $value) {
 			$data = curl_helper::curl($url);
 			$helper = new map_helper();
 			$helper->handle_data($data, $store_category);
-			$location_a_x++;
-			$location_b_x++;
+			$location_a_x += 6;
+			$location_b_x += 6;
 			$j++;
 		}
-		$location_a_y++;
-		$location_b_y++;
+		$location_a_y += 4;
+		$location_b_y += 4;
 		$i++;
 	}
 }
